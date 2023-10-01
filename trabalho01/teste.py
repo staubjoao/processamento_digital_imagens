e5d8f10b40a5389dbfb4af47ce27a503f99eb1ca
staubjoao <joao.staub42@gmail.com>
2023-10-01 19:36:49 -0300

    Finalizado

--- trabalho01/teste.py
+++ trabalho01/teste.py
@@ -9,7 +9,9 @@ x = 2
 limite_superior = ((m + x) / 2) % 180
 limite_inferior = ((m - x) / 2) % 180
 
-h, s , v = cv2.split(image_hsv)
+h = image_hsv[:, :, 0]
+s = image_hsv[:, :, 1]
+v = image_hsv[:, :, 2]
 
 inicio = h >= limite_inferior
 final = h <= limite_superior
@@ -18,3 +20,7 @@ h = h.astype(np.uint16)
 
 operacao = inicio & final if limite_inferior < limite_superior else inicio | final
 h[operacao] = np.mod(h[operacao] + 90, 180).astype(np.uint8)
+
+h = h.astype(np.uint8)
+
+image_hsv = cv2.merge([h, s, v])
